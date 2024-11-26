836b54ce8233508e0b149f435a0617d5d02cb912
Benjamin LaGreca <benlagreca02@gmail.com>
2024-11-25 22:14:00 -0500

    a bit more stubbing, compiles, haven't tried to run

--- marge/include/marge/marge.hpp
+++ marge/include/marge/marge.hpp
@@ -8,17 +8,32 @@ namespace marge {
 class Marge : public controller_interface::ControllerInterface {
 public:
 
+    // A LOT of other code uses these
+    // I belive they're for/from visibility_control.h which I am not doing
+    // becuase it seems dumb
+    //
+    // Its for controlling visibility of the functions? but i don't care
+    // Nothing matters anymore, I'm gonna start pronouncing the L in salmon
+    //
+    // CONTROLLER_INTERFACE_PUBLIC
     Marge();
-    ~Marge();
 
-    /*
-        command_interface_configuration
-        state_insterface_configuration
-        on_configure
-        on_activate
-        on_deactivate
-        update
-    */
+
+    controller_interface::InterfaceConfiguration command_interface_configuration() const override;
+
+
+    controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+
+
+    controller_interface::CallbackReturn on_init() override;
+
+    controller_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State & previous_state) override;
+
+    controller_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
+
+    controller_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
+
+    // controller_interface::CallbackReturn update(const rclcpp_lifecycle::State & previous_state) override;
         
 
 
--- marge/src/marge.cpp
+++ marge/src/marge.cpp
@@ -1,9 +1,25 @@
 #include "marge/marge.hpp"
 
+#include <iostream>
 
 namespace marge {
 
     Marge::Marge(){};
+
+
+    controller_interface::CallbackReturn Marge::on_init(){
+        // Do some stuff
+        std::cout << "HELLO WORLD, MARGE HAS BEEN INIT" << std::endl;
+
+        // ROS node garbage here
+
+        // This is what tutorial says to return but...
+        // return controller_interface::return_type::OK; 
+
+        // This is what the example code does
+        return CallbackReturn::SUCCESS;
+    }
+    
 
 
 };
