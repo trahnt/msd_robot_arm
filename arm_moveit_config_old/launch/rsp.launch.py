62c817013b3faa3fd3dedcfd4accd541391265bd
Benjamin LaGreca <benlagreca02@gmail.com>
2024-11-28 10:38:24 -0500

    Added fake gripper to the arm as the _sim.urdf

--- arm_moveit_config_old/launch/rsp.launch.py
+++ arm_moveit_config_old/launch/rsp.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_rsp_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("arm_end_effector", package_name="arm_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("arm_end_effector", package_name="arm_moveit_config_old").to_moveit_configs()
     return generate_rsp_launch(moveit_config)
