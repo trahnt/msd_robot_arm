f7146000f78e9fe823c6cd062daee1d03f3caa83
Benjamin LaGreca <benlagreca02@gmail.com>
2024-11-30 12:35:35 -0500

    ocmmented out the got read/write update messages bc they clogged terminal

--- arm_motor_controller/src/ArmSystem.cpp
+++ arm_motor_controller/src/ArmSystem.cpp
@@ -255,8 +255,10 @@ hardware_interface::CallbackReturn ArmSystemHardware::on_shutdown(const rclcpp_l
 hardware_interface::return_type ArmSystemHardware::read(const rclcpp::Time &time, const rclcpp::Duration &period) {
     static double lastupdate = 0.0;
     if (time.seconds() - lastupdate > 1) {
+        /*
         RCLCPP_INFO(rclcpp::get_logger("ArmController"), "Got read update! Current time: %f, Period: %f",
                     time.seconds(), period.seconds());
+                    */
         lastupdate = time.seconds();
     }
 
@@ -269,8 +271,10 @@ hardware_interface::return_type ArmSystemHardware::read(const rclcpp::Time &time
 hardware_interface::return_type ArmSystemHardware::write(const rclcpp::Time &time, const rclcpp::Duration &period) {
     static double lastupdate = 0.0;
     if (time.seconds() - lastupdate > 1) {
+        /*
         RCLCPP_INFO(rclcpp::get_logger("ArmController"), "Got write update! Current time: %f, Period: %f",
                     time.seconds(), period.seconds());
+                    */
         lastupdate = time.seconds();
     }
 
