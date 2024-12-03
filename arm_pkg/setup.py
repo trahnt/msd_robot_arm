76bf4719d4403c42c8f5d210645ff491b976136e
Benjamin LaGreca <benlagreca02@gmail.com>
2024-12-03 02:26:22 -0500

    ITS DOING STUFF WIT HTHE MOVEIT

--- arm_pkg/setup.py
+++ arm_pkg/setup.py
@@ -1,15 +1,19 @@
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = 'arm_pkg'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.6.9',
     packages=find_packages(exclude=['test']),
+    # add other files to 'install' here
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, glob('launch/*.py')),
+        ('share/' + package_name+'/urdf/', glob('urdf/*')),
+        ('share/' + package_name+'/config/', glob('config/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
